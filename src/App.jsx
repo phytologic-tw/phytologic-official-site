9cead16b98e76d2a35e917d0c82ee83165104650
phytologic-tw <bryan@phytologic.tw>
2026-05-16 14:24:52 +0800

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -2,34 +2,17 @@ export default function App() {
   return (
     <div
       style={{
+        background: "black",
         minHeight: "100vh",
-        background: "#07130d",
-        color: "white",
         display: "flex",
         justifyContent: "center",
         alignItems: "center",
-        flexDirection: "column",
-        fontFamily: "sans-serif",
+        color: "white",
+        fontSize: "100px",
+        fontWeight: 900,
       }}
     >
-      <h1
-        style={{
-          fontSize: "100px",
-          margin: 0,
-          fontWeight: 900,
-        }}
-      >
-        PHYTOLOGIC V3
-      </h1>
-
-      <p
-        style={{
-          fontSize: "32px",
-          opacity: 0.7,
-        }}
-      >
-        AI HEALTH SYSTEM
-      </p>
+      TEST SUCCESS
     </div>
   );
 }
